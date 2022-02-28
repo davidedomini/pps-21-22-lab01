00b69c0f0345fc0b422d1e00a4c0e302b350dcf4
dom <davidedomini@gmail.com>
2022-02-28 11:04:11 +0100

    added the two new classes for the implementation of BankAccountWithATM and the two first tests

---
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,39 @@
+package lab01.example.model;
+
+public class SimpleBankAccountWithAtm implements BankAccount {
+
+    private final static int FEE = 1; //1$ fee on each transaction
+    private double balance;
+    private final AccountHolder accountHolder;
+
+    public SimpleBankAccountWithAtm(AccountHolder accountHolder, int balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    @Override
+    public AccountHolder getHolder() {
+        return this.accountHolder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(int userID, double amount) {
+        if (checkUser(userID)) {
+            this.balance = this.balance + amount - FEE;
+        }
+    }
+
+    @Override
+    public void withdraw(int userID, double amount) {
+
+    }
+
+    private boolean checkUser(final int id) {
+        return this.accountHolder.getId() == id;
+    }
+}
---
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,34 @@
+import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
+import lab01.example.model.SimpleBankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleBankAccountWithAtmTest {
+
+    private AccountHolder accountHolder;
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    void beforeEach(){
+        accountHolder = new AccountHolder("Mario", "Rossi", 1);
+        bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
+    }
+
+    @Test
+    void testInitialBalance() {
+        assertEquals(0, bankAccount.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        bankAccount.deposit(accountHolder.getId(), 100);
+        assertEquals(99, bankAccount.getBalance());
+    }
+
+
+
+}
