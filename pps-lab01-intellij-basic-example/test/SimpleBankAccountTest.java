9a114b49a3b712a21a8c9331a5b2b820c1c16a6e
dom <davidedomini@gmail.com>
2022-03-01 14:49:45 +0100

    refactored tests adding a new abstract superclass

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
@@ -1,54 +1,22 @@
 import lab01.example.model.AccountHolder;
-import lab01.example.model.BankAccount;
 import lab01.example.model.SimpleBankAccount;
-
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  * The test suite for testing the SimpleBankAccount implementation
  */
 
-class SimpleBankAccountTest {
-
-    private AccountHolder accountHolder;
-    private BankAccount bankAccount;
+class SimpleBankAccountTest extends BankAccountTest {
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new SimpleBankAccount(accountHolder, 0);
     }
 
-    @Test
-    void testInitialBalance() {
-        assertEquals(0, bankAccount.getBalance());
-    }
-
-    @Test
-    void testDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(100, bankAccount.getBalance());
-    }
-
-    @Test
-    void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.deposit(2, 50);
-        assertEquals(100, bankAccount.getBalance());
-    }
-
-    @Test
-    void testWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(accountHolder.getId(), 70);
-        assertEquals(30, bankAccount.getBalance());
-    }
-
-    @Test
-    void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(2, 70);
-        assertEquals(100, bankAccount.getBalance());
+    @Override
+    protected int getFee() {
+        return 0;
     }
 }
--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -1,53 +1,17 @@
 import lab01.example.model.AccountHolder;
-import lab01.example.model.BankAccount;
-import lab01.example.model.SimpleBankAccount;
 import lab01.example.model.SimpleBankAccountWithAtm;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class SimpleBankAccountWithAtmTest {
-
-    private AccountHolder accountHolder;
-    private BankAccount bankAccount;
+public class SimpleBankAccountWithAtmTest extends BankAccountTest {
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new SimpleBankAccountWithAtm(0, accountHolder);
     }
 
-    @Test
-    void testInitialBalance() {
-        assertEquals(0, bankAccount.getBalance());
+    @Override
+    protected int getFee() {
+        return 1;
     }
-
-    @Test
-    void testDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(99, bankAccount.getBalance());
-    }
-
-    @Test
-    void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.deposit(2, 50);
-        assertEquals(99, bankAccount.getBalance());
-    }
-
-    @Test
-    void testWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(accountHolder.getId(), 70);
-        assertEquals(28, bankAccount.getBalance());
-    }
-
-    @Test
-    void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(2, 70);
-        assertEquals(99, bankAccount.getBalance());
-    }
-
 }
